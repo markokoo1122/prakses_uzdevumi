6fa515ff24c5f6df3246d2837ba593193181d1ca
marko <mariussmarko1122@gmail.com>
2026-03-05 20:54:35 +0200

    main page

--- app/bedrock/web/app/themes/zonda-theme/resources/views/front-page.blade.php
+++ app/bedrock/web/app/themes/zonda-theme/resources/views/front-page.blade.php
@@ -2,29 +2,152 @@
 
 <main class="main-content">
 
-<div class="front-page-container">
+    <div class="front-page-container">
 
-    <div class="hero-section">
+        <div class="hero-section">
 
-        <div class="hero-content">
+            <div class="hero-content">
 
-            <img width="650" height="auto" src="<?php echo get_template_directory_uri(); ?>/resources/images/banner.png"
-                alt="Zonda Lighting Logo">
+                <img width="650" height="auto"
+                    src="<?php echo get_template_directory_uri(); ?>/resources/images/banner.png"
+                    alt="Zonda Lighting Logo">
+
+            </div>
+
+            <div class="hero-text">
+
+                <p style="text-align: left;">We provide designers with minimalist products with a multitude of options.
+                    Pure shapes, clean lines and gentle apertures that deliver precise performance where needed.</p>
+
+
+            </div>
 
         </div>
 
-        <div class="hero-text">
+        <div class="products-section">
 
-            <p style="text-align: left;">We provide designers with minimalist products with a multitude of options. Pure shapes, clean lines and gentle apertures that deliver precise performance where needed.</p>
-            
+            <div class="product-card">
 
+                <img width="420" height="420"
+                    src="<?php echo get_template_directory_uri(); ?>/resources/images/lamp_type1.jpg"
+                    alt="Zonda Lighting Logo">
+                <hr>
+                <p>systems</p>
+
+            </div>
+
+            <div class="product-card">
+
+                <img width="420" height="420"
+                    src="<?php echo get_template_directory_uri(); ?>/resources/images/lamp_type2.png"
+                    alt="Zonda Lighting Logo">
+                <hr>
+                <p>round luminaires</p>
+
+            </div>
+
+            <div class="product-card">
+
+                <img width="420" height="420"
+                    src="<?php echo get_template_directory_uri(); ?>/resources/images/lamp_type3.png"
+                    alt="Zonda Lighting Logo">
+                <hr>
+                <p>linear luminaires</p>
+
+            </div>
+
+            <div class="product-card">
+
+                <img width="420" height="420"
+                    src="<?php echo get_template_directory_uri(); ?>/resources/images/lamp_type4.png"
+                    alt="Zonda Lighting Logo">
+                <hr>
+                <p>surface luminaires</p>
+
+            </div>
+
+            <div class="product-card">
+
+                <img width="420" height="420"
+                    src="<?php echo get_template_directory_uri(); ?>/resources/images/lamp_type5.png"
+                    alt="Zonda Lighting Logo">
+                <hr>
+                <p>recessed luminaires</p>
+
+            </div>
+
+            <div class="product-card">
+
+                <img width="420" height="420"
+                    src="<?php echo get_template_directory_uri(); ?>/resources/images/lamp_type6.png"
+                    alt="Zonda Lighting Logo">
+                <hr>
+                <p>trimless luminaires</p>
+
+            </div>
+
+        </div>
+
+        <div class="button-container">
+            <a href="#" class="btn">all products</a>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="images-container">
+
+            <div class="tall-image">
+
+                <img src="<?php echo get_template_directory_uri(); ?>/resources/images/tall.jpeg" alt="Tall Image">
+                <div class="label">FADING</div>
+
+            </div>
+
+            <div class="image-tower">
+
+                <div class="tower1">
+
+                    <img src="<?php echo get_template_directory_uri(); ?>/resources/images/tower-img1.jpg"
+                        alt="Tower Top">
+                    <div class="label">ALLORA</div>
+
+                </div>
+
+                <div class="tower2">
+
+                    <img src="<?php echo get_template_directory_uri(); ?>/resources/images/tower-img2.jpeg"
+                        alt="Tower Bottom">
+                    <div class="label">GOBI</div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+        <div class="button-container">
+            <a href="#" class="btn">more</a>
+        </div>
+
+        <div class="end-section">
+            <div class="end-text">
+                <div class="e-text">
+                <p>True to our values and vision and with an eye for our high quality standards in our products, in our
+                    designs and in our service to our customers.<br><br>
+                    We strive for the highest quality in each of our lighting tools that we develop and deliver.<br><br>
+                    Our knowledge and operation enable us to propose products and solutions customized for each project.
+                </div>
+                </p>
+            </div>
+            <br>
+            <div>
+                <img src="<?php echo get_template_directory_uri(); ?>/resources/images/end.png" alt="Bottom image">
+            </div>
         </div>
 
     </div>
-
-    
-
-</div>
 
 </main>
 
